b909a3deb692b006009918981718f7a6856dfcbe
Administrator <Administrator@t450-PC>
2018-05-22 11:34:38 +0800

    字数统计

---
+++ src/main/java/com/xiaoshabao/example/module/base/字符编码/CharsetTest.java
@@ -0,0 +1,26 @@
+package com.xiaoshabao.example.module.base.字符编码;
+
+import org.junit.Test;
+
+public class CharsetTest {
+
+	/**
+	 * 统计汉字
+	 */
+	@Test
+	public void countFont() {
+		String str="我是1个人。";
+		char[] arrays=str.toCharArray();
+		int count = 0;
+		String regex="[\\u4E00-\\u9FA5]|[0-9]+";
+		for (int i = 0; i <arrays.length ; i++) {
+			if (Character.toString(arrays[i]).matches(regex)) {
+				count++;
+			}
+		}
+		
+		System.out.println("总计字数："+count);
+
+	}
+
+}
