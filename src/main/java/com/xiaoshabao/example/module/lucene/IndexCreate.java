0c1cc2c579769dd6a8e66220e24956c84e2452b8
Administrator <Administrator@t450-PC>
2018-05-29 15:51:27 +0800

    lucene的简易使用

--- src/main/java/com/xiaoshabao/example/module/lucene/IndexCreate.java
+++ src/main/java/com/xiaoshabao/example/module/lucene/IndexCreate.java
@@ -8,6 +8,8 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
@@ -17,40 +19,48 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
 
-
 public class IndexCreate {
-	
+
 	/**
 	 * 创建
 	 */
 	@Test
 	public void test1() {
-		//分词技术(标准分词)
-		Analyzer analyzer=new StandardAnalyzer();
-		//索引配置信息
-		IndexWriterConfig indexWriterConfig=new IndexWriterConfig(analyzer);
-		//索引打开方式（没有新建，有打开）
+		// 分词技术(标准分词)
+		Analyzer analyzer = new StandardAnalyzer();
+		// 索引配置信息
+		IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+		// 索引打开方式（没有新建，有打开）
 		indexWriterConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
 		//
-		Directory directory=null;
-		IndexWriter writer=null;
+		Directory directory = null;
+		IndexWriter writer = null;
 		try {
-			directory=FSDirectory.open(Paths.get("E:\\test\\indexs"));
+			directory = FSDirectory.open(Paths.get("E:\\test\\indexs"));
 			writer = new IndexWriter(directory, indexWriterConfig);
-			
-			//创建文档
+
+			// 创建文档
 			Document doc = new Document();
-			//创建域
-			doc.add(new IntPoint("id",1));
-			doc.add(new TextField("content","测试内容",Field.Store.YES));
-			doc.add(new StringField("remark","备注一下",Field.Store.YES));
-			writer.addDocument(doc);
+			// 创建域,使用IntPoint时需要同时创建同名NumericDocValuesField（排序用）以及同名StoredField对象
+			doc.add(new IntPoint("id",1));  
+	        doc.add(new StoredField("id",1));  
+			doc.add(new NumericDocValuesField("id", 1));
 			
+			doc.add(new TextField("content", "测试内容", Field.Store.YES));
+			doc.add(new StringField("remark", "备注一下", Field.Store.YES));
+			//只添加，多次调用会出现多次结果，查询时会循环多次
+			writer.addDocument(doc);
+			//更新，根据term创建的参数更新
+//			writer.updateDocument(new Term("id"), doc);
+
 			writer.commit();
 			writer.close();
+			directory.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("索引创建成功！");
 	}
 
 }
--- src/main/java/com/xiaoshabao/example/module/lucene/IndexSearch.java
+++ src/main/java/com/xiaoshabao/example/module/lucene/IndexSearch.java
@@ -1,11 +1,61 @@
 package com.xiaoshabao.example.module.lucene;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.junit.Test;
 
 public class IndexSearch {
 	
 	@Test
 	public void test1() {
+		Directory directory=null;
+		try {
+			directory=FSDirectory.open(Paths.get("E:\\test\\indexs"));
+			//读取索引文件
+			IndexReader reader = DirectoryReader.open(directory);
+			IndexSearcher searcher = new IndexSearcher(reader);
+			//分词技术，要和创建索引用的一样
+			Analyzer analyzer=new StandardAnalyzer();
+			
+			//请求，限定搜索字段
+			QueryParser parser = new QueryParser("content", analyzer);
+			//设置搜索关键字
+			Query query = parser.parse("测试");
+			//搜索前10条
+			TopDocs results = searcher.search(query, 10);
+			
+			//结果输出
+			//结果数组
+			ScoreDoc[] hits = results.scoreDocs;
+			for(int i=0;i<hits.length;i++) {
+				Document doc = searcher.doc(hits[i].doc);
+				System.out.println("文档id："+doc.get("id"));
+				System.out.println("文档content："+doc.get("content"));
+				System.out.println("文档remark："+doc.get("remark"));
+			}
+			directory.close();
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("索引读取错误");
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.out.println("解析关键字错误");
+		}
 		
 	}
 
