40b7d29e32581fcdb9cd962fc2a98c5761a577f1
manxx <manxx@yonyou.com>
2019-08-02 13:42:31 +0800

    参数标识

---
+++ src/main/java/com/xiaoshabao/example/module/验证注解/ParamsAnnotation.java
@@ -0,0 +1,25 @@
+package com.xiaoshabao.example.module.验证注解;
+
+
+import org.junit.Test;
+import org.springframework.lang.NonNull;
+
+
+/**
+ * 参数验证
+ */
+public class ParamsAnnotation {
+  
+  @Test
+  public void test() {
+    testNotNull(null);
+  }
+  
+  /**
+   * 做标识 @NonNull 不可以为空  @Nullable可以为可空
+   */
+  public void testNotNull(@NonNull String name) {
+    System.out.println("通过测试："+name);
+  }
+
+}
